20cf2e7ec280605f40e02bc8f725e497fa80b054
harsen <harsen@tapdata.io>
2024-03-12 20:52:44 +0800

    fix: #149144 add UnitTestUtilsTest.injectField testcase.

--- plugin-kit/tapdata-api/src/test/java/io/tapdata/utils/UnitTestUtilsTest.java
+++ plugin-kit/tapdata-api/src/test/java/io/tapdata/utils/UnitTestUtilsTest.java
@@ -1,6 +1,7 @@
 package io.tapdata.utils;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class UnitTestUtilsTest {
@@ -9,5 +10,59 @@ class UnitTestUtilsTest {
 	@Test
 	void testIsTesting() {
 		Assertions.assertTrue(UnitTestUtils.isTesting());
+	}
+
+	@Nested
+	class InjectFieldTest {
+
+		@Test
+		void shouldSetFieldValue() {
+			// Given
+			TestClass testClass = new TestClass();
+			String fieldName = "field";
+			String expectedValue = "newValue";
+
+			// When
+			UnitTestUtils.injectField(TestClass.class, testClass, fieldName, expectedValue);
+
+			// Then
+			Assertions.assertEquals(expectedValue, testClass.getField());
+		}
+
+		@Test
+		void shouldThrowExceptionWhenFieldDoesNotExist() {
+			// Given
+			TestClass testClass = new TestClass();
+			String fieldName = "nonExistentField";
+			String value = "newValue";
+
+			// Then
+			Assertions.assertThrows(RuntimeException.class, () -> {
+				// When
+				UnitTestUtils.injectField(TestClass.class, testClass, fieldName, value);
+			});
+		}
+
+		@Test
+		void shouldThrowExceptionWhenValueIsIncompatible() {
+			// Given
+			TestClass testClass = new TestClass();
+			String fieldName = "field";
+			int incompatibleValue = 123;
+
+			// Then
+			Assertions.assertThrows(RuntimeException.class, () -> {
+				// When
+				UnitTestUtils.injectField(TestClass.class, testClass, fieldName, incompatibleValue);
+			});
+		}
+
+		class TestClass {
+			private String field;
+
+			public String getField() {
+				return field;
+			}
+		}
 	}
 }
