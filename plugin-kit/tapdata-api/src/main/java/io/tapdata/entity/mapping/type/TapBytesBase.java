b3dcc324809779d4a6322911378b24c8a0b4e99e
harsen <harsen@tapdata.io>
2024-04-18 22:37:44 +0800

    fix: Fixed schema transform char length not work.

--- plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/mapping/type/TapBytesBase.java
+++ plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/mapping/type/TapBytesBase.java
@@ -25,28 +25,23 @@ public abstract class TapBytesBase extends TapMapping {
     protected Integer byteRatio;
 
     protected Long getFromTapTypeBytes(Long comingBytes, Integer comingByteRatio) {
-//        if(this.byteRatio == 1)
-//            return bytes;
-//        // 14 / 8 = 2, 14 / 7 = 2
-//        return (bytes / this.byteRatio + ((bytes % this.byteRatio) > 0 ? 1 : 0));
+			if (null == comingByteRatio) {
+				// char(comingBytes) --> char(comingBytes)
+				if (null == this.byteRatio) return comingBytes;
 
-        if (null == comingByteRatio) {
-            if (null != this.byteRatio) {
-							if (comingBytes % this.byteRatio > 0) {
-								return comingBytes / this.byteRatio + 1;
-							}
-                return comingBytes / this.byteRatio;
-            }
-            return comingBytes;
-        } else if (null == this.byteRatio) {
-            return comingBytes * comingByteRatio;
-        } else {
-            long result = comingBytes * comingByteRatio;
-            if (result % this.byteRatio > 0) {
-                return result / this.byteRatio + 1;
-            }
-            return result / this.byteRatio;
-        }
+				// char(comingBytes) --> byte(comingBytes*this.byteRatio)
+				return comingBytes * this.byteRatio;
+			}
+
+			// byte(comingBytes) --> char(comingBytes)
+			if (null == this.byteRatio) return comingBytes;
+
+			// byte(comingBytes) --> byte(comingBytes*comingByteRatio/this.byteRatio+%pad)
+			long results = comingBytes * comingByteRatio;
+			if (results % this.byteRatio > 0) {
+				return results / this.byteRatio + 1;
+			}
+			return results / this.byteRatio;
     }
 
     protected Long getToTapTypeBytes(Map<String, String> params) {
