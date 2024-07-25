46aa45799b6b5a6454a1084dc15da7ac02e1a49c
sam.p <102567930@qq.com>
2024-07-25 17:55:48 +0800

    perf: Optimize performance
    Closes TAP-3592

--- plugin-kit/tapdata-common/src/main/java/io/tapdata/common/concurrent/SimpleConcurrentProcessorImpl.java
+++ plugin-kit/tapdata-common/src/main/java/io/tapdata/common/concurrent/SimpleConcurrentProcessorImpl.java
@@ -29,4 +29,24 @@ public class SimpleConcurrentProcessorImpl<T, R> extends BaseConcurrentProcessor
 	public boolean runAsync(T input, Function<T, R> function, long timeout, TimeUnit timeUnit) {
 		return super.produce(input, function, timeout, timeUnit);
 	}
+
+	@Override
+	public void runAsyncWithBlocking(T input, Function<T, R> function) {
+		synchronized (this.produceLock) {
+			super.produce(input, function);
+			super.putBarrierInAllProducerQueue();
+		}
+	}
+
+	@Override
+	public boolean runAsyncWithBlocking(T input, Function<T, R> function, long timeout, TimeUnit timeUnit) {
+		synchronized (this.produceLock) {
+			boolean produce = super.produce(input, function, timeout, timeUnit);
+			if (Boolean.TRUE.equals(produce)) {
+				super.putBarrierInAllProducerQueue();
+				return true;
+			}
+		}
+		return false;
+	}
 }
---
+++ plugin-kit/tapdata-common/src/main/java/io/tapdata/common/concurrent/ThreadTask.java
@@ -0,0 +1,9 @@
+package io.tapdata.common.concurrent;
+
+/**
+ * @author samuel
+ * @Description
+ * @create 2024-07-25 15:26
+ **/
+public abstract class ThreadTask<T, R> {
+}
