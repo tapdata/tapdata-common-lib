298afcb76059f1a0ecaea37d5dee36f90e5a8cfd
jarad0628 <coolgj@163.com>
2024-08-15 12:57:06 +0800

    fix: change insert policy multi-thread bug

--- plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/ConnectorCapabilities.java
+++ plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/ConnectorCapabilities.java
@@ -1,26 +1,42 @@
 package io.tapdata.pdk.apis.entity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectorCapabilities {
     public static ConnectorCapabilities create() {
         return new ConnectorCapabilities();
     }
+
     private List<String> disableCapabilities;
+
     public ConnectorCapabilities disable(String capabilityId) {
-        if(disableCapabilities == null)
+        if (disableCapabilities == null)
             disableCapabilities = new ArrayList<>();
         disableCapabilities.add(capabilityId);
         return this;
     }
+
     private Map<String, String> capabilityAlternativeMap;
-    public ConnectorCapabilities alternative(String capabilityId, String alternative) {
-        if(capabilityAlternativeMap == null)
-            capabilityAlternativeMap = new HashMap<>();
-        capabilityAlternativeMap.put(capabilityId, alternative);
+    private Map<String, Map<String, String>> capabilityAlternativeThreadMap;
+
+    public synchronized ConnectorCapabilities alternative(String capabilityId, String alternative) {
+        if (capabilityAlternativeMap == null) {
+            capabilityAlternativeMap = new ConcurrentHashMap<>();
+            capabilityAlternativeMap.put(capabilityId, alternative);
+        }
+        if (capabilityAlternativeThreadMap == null) {
+            capabilityAlternativeThreadMap = new ConcurrentHashMap<>();
+        }
+        if (capabilityAlternativeThreadMap.containsKey(Thread.currentThread().getName())) {
+            capabilityAlternativeThreadMap.get(Thread.currentThread().getName()).put(capabilityId, alternative);
+        } else {
+            Map<String, String> threadMap = new ConcurrentHashMap<>();
+            threadMap.put(capabilityId, alternative);
+            capabilityAlternativeThreadMap.put(Thread.currentThread().getName(), threadMap);
+        }
         return this;
     }
 
@@ -29,7 +45,9 @@ public class ConnectorCapabilities {
     }
 
     public String getCapabilityAlternative(String capabilityId) {
-        if(capabilityAlternativeMap != null)
+        if (capabilityAlternativeThreadMap != null && capabilityAlternativeThreadMap.containsKey(Thread.currentThread().getName()))
+            return capabilityAlternativeThreadMap.get(Thread.currentThread().getName()).get(capabilityId);
+        if (capabilityAlternativeMap != null)
             return capabilityAlternativeMap.get(capabilityId);
         return null;
     }
