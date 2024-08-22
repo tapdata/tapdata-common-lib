328341b692309bce7dc04bf554437887903c7bb0
Gavin'Xiao <2749984520@qq.com>
2024-08-22 16:42:41 +0800

    fix(queryHashByAdvanceFilter): queryHashByAdvanceFilterFunction show extends TapConnectorFunction

--- plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/functions/connector/common/QueryHashByAdvanceFilterFunction.java
+++ plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/functions/connector/common/QueryHashByAdvanceFilterFunction.java
@@ -3,10 +3,11 @@ package io.tapdata.pdk.apis.functions.connector.common;
 import io.tapdata.entity.schema.TapTable;
 import io.tapdata.pdk.apis.context.TapConnectorContext;
 import io.tapdata.pdk.apis.entity.TapAdvanceFilter;
+import io.tapdata.pdk.apis.functions.connector.TapConnectorFunction;
 import io.tapdata.pdk.apis.functions.connector.common.vo.TapHashResult;
 
 import java.util.function.Consumer;
 
-public interface QueryHashByAdvanceFilterFunction {
+public interface QueryHashByAdvanceFilterFunction extends TapConnectorFunction {
     public void query(TapConnectorContext connectorContext, TapAdvanceFilter filter, TapTable table, Consumer<TapHashResult<String>> consumer) throws Throwable;
 }
