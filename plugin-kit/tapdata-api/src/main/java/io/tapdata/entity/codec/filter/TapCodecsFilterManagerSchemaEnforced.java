cc47054537198f908428b67a4469359f48924405
jarad0628 <coolgj@163.com>
2024-10-08 11:25:36 +0800

    Revert "fix: tapdata-api add TapBinary default"
    This reverts commit c1d8433be599c828ac86c036b2e38d8efda0ca81.

--- plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/filter/TapCodecsFilterManagerSchemaEnforced.java
+++ plugin-kit/tapdata-api/src/main/java/io/tapdata/entity/codec/filter/TapCodecsFilterManagerSchemaEnforced.java
@@ -118,10 +118,6 @@ public class TapCodecsFilterManagerSchemaEnforced extends TapCodecsFilterManager
 									break;
 								case TapType.TYPE_NUMBER:
 									if (!(value instanceof Number)) isTypeQualified = false;
-									break;
-								case TapType.TYPE_BINARY:
-									if (!(value instanceof byte[])) isTypeQualified = false;
-									break;
 								default:
 									break;
 							}
