71803d09b093cad8bfbe1c95adf04b73158653e7
mnianqi <mnq0626@163.com>
2024-12-02 19:32:02 +0800

    feat(iengine): TAP-5906 code review

--- error-code-root/error-code-core/src/test/java/io/tapdata/utils/ErrorCodeUtilsTest.java
+++ error-code-root/error-code-core/src/test/java/io/tapdata/utils/ErrorCodeUtilsTest.java
@@ -1,10 +1,8 @@
 package io.tapdata.utils;
 
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ErrorCodeUtilsTest {
     @Test
