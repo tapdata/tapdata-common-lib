e1d929c8158b7f5a9eef5592fb073b0bc6f0c270
mnianqi <mnq0626@163.com>
2024-12-02 18:29:55 +0800

    feat(iengine): TAP-5906 code review

--- plugin-kit/tapdata-pdk-runner/src/main/java/io/tapdata/pdk/core/error/TapPdkRunnerExCode_18.java
+++ plugin-kit/tapdata-pdk-runner/src/main/java/io/tapdata/pdk/core/error/TapPdkRunnerExCode_18.java
@@ -21,8 +21,8 @@ public interface TapPdkRunnerExCode_18 {
 	)
 	String CALL_ERROR_HANDLE_API_ERROR = "18002";
 	@TapExCode(
-			describe = "The exception class loader thrown from the PDK connector is incorrect, please confirm whether the scope of the pdk error code dependency is provided",
-			describeCN = "数据源中抛出的异常类加载器不正确，请确认PDK错误码依赖是否范围是provided",
+			describe = "The exception class loader thrown from the PDK connector is incorrect",
+			describeCN = "数据源中抛出的异常类加载器不正确",
 			dynamicDescription = "Received exception class loader: {}, expected: {}",
 			dynamicDescriptionCN = "接收到的类加载器类型为: {}, 期望的类加载器类型为: {}"
 	)
