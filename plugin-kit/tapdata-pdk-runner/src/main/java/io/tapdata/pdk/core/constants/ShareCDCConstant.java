13a85c523641836fa9afdf181eccabbbb7dc05b0
sam.p <102567930@qq.com>
2024-12-31 17:25:29 +0800

    feat: Add a configuration to adjust the sequence frequency of refreshing the mongodb ringbuffer through environment variables or system properties
    Closes TAP-5418

---
+++ plugin-kit/tapdata-pdk-runner/src/main/java/io/tapdata/pdk/core/constants/ShareCDCConstant.java
@@ -0,0 +1,15 @@
+package io.tapdata.pdk.core.constants;
+
+/**
+ * @author samuel
+ * @Description
+ * @create 2024-12-31 10:58
+ **/
+public interface ShareCDCConstant {
+	/**
+	 * Share cdc delay mode
+	 */
+	String DELAY_MODE_PROP_KEY = "share_cdc_delay_mode";
+	int DELAY_MODE_DEFAULT = 0;
+	int DELAY_MODE_LOW = 1;
+}
