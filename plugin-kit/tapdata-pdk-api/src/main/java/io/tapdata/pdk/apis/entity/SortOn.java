45c8665635874ba5efba5c436f1801ad4623755b
ljvv7 <fes504200361@qq.com>
2025-03-19 13:18:14 +0800

    TAP-5866 feat:sort on support null null sort

--- plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/SortOn.java
+++ plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/SortOn.java
@@ -3,14 +3,24 @@ package io.tapdata.pdk.apis.entity;
 public class SortOn {
     public static final int ASCENDING = 1;
     public static final int DESCENDING = 2;
+    public static final int DEFAULT_NULL_SORT = 0;
+    public static final int NULLS_FIRST = 1;
+    public static final int NULLS_LAST = 2;
     private String key;
     private int sort;
+    private int nullSort;
 
     public SortOn() {}
-    
+
     public SortOn(String key, int sort) {
         this.key = key;
         this.sort = sort;
+    }
+
+    public SortOn(String key, int sort, int nullSort) {
+        this.key = key;
+        this.sort = sort;
+        this.nullSort = nullSort;
     }
 
     public static SortOn ascending(String key) {
@@ -37,11 +47,20 @@ public class SortOn {
         this.sort = sort;
     }
 
+    public int getNullSort() {
+        return nullSort;
+    }
+
+    public void setNullSort(int nullSort) {
+        this.nullSort = nullSort;
+    }
+
+
     public String toString() {
         return toString("");
     }
 
     public String toString(String quote) {
-        return quote + key + quote + " " + (sort == ASCENDING ? "ASC" : "DESC");
+        return quote + key + quote + " " + (sort == ASCENDING ? "ASC" : "DESC") ;
     }
 }
