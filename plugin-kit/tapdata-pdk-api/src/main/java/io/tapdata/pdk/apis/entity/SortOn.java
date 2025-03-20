b680b9104912344fd5d537636b03d84472ac3fa4
ljvv7 <fes504200361@qq.com>
2025-03-20 15:11:05 +0800

    fix :add null sort

--- plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/SortOn.java
+++ plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/entity/SortOn.java
@@ -3,14 +3,24 @@ package io.tapdata.pdk.apis.entity;
 public class SortOn {
     public static final int ASCENDING = 1;
     public static final int DESCENDING = 2;
+    public static final int DEFAULT_NULL_SORT = 0;
+    public static final int NULLS_FIRST = 1;
+    public static final int NULLS_LAST = 2;
     private String key;
     private int sort;
+    private int nullSort;
 
     public SortOn() {}
-    
+
     public SortOn(String key, int sort) {
         this.key = key;
         this.sort = sort;
+    }
+
+    public SortOn(String key, int sort, int nullSort) {
+        this.key = key;
+        this.sort = sort;
+        this.nullSort = nullSort;
     }
 
     public static SortOn ascending(String key) {
@@ -35,6 +45,13 @@ public class SortOn {
 
     public void setSort(int sort) {
         this.sort = sort;
+    }
+    public int getNullSort() {
+        return nullSort;
+    }
+
+    public void setNullSort(int nullSort) {
+        this.nullSort = nullSort;
     }
 
     public String toString() {
