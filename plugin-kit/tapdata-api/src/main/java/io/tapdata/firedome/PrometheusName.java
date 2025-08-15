0aee09f4ebc9e0363d592a42af66e6971aff2f0c
sam.p <102567930@qq.com>
2025-08-15 16:01:06 +0800

    feat(iengine): Add a series of task-related prometheus metrics

---
+++ plugin-kit/tapdata-api/src/main/java/io/tapdata/firedome/PrometheusName.java
@@ -0,0 +1,20 @@
+package io.tapdata.firedome;
+
+/**
+ * @author samuel
+ * @Description
+ * @create 2025-08-15 15:49
+ **/
+public interface PrometheusName {
+	/* task */
+	String TASK_CDC_DELAY_SECONDS = "task_cdc_delay_seconds";
+	String TASK_ACTIVE_DB = "task_active_db";
+	String TASK_STATUS = "task_status";
+	String TASK_MILESTONE_STATUS = "task_milestone_status";
+
+	/* node */
+	String TASK_NODE_PROCESS_DATA_MS = "task_node_process_data_ms";
+
+	/* inspect task */
+	String TASK_VALIDATION_STATUS = "task_validation_status";
+}
