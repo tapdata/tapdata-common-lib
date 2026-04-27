6ed0c6497bbc11ba67bb704837a5ff869de8cade
jarad0628 <coolgj@163.com>
2026-04-27 11:36:16 +0800

    feat: add consumeMessage api

--- plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/functions/connector/source/ConsumeMessageFunction.java
+++ plugin-kit/tapdata-pdk-api/src/main/java/io/tapdata/pdk/apis/functions/connector/source/ConsumeMessageFunction.java
@@ -2,8 +2,9 @@ package io.tapdata.pdk.apis.functions.connector.source;
 
 import io.tapdata.entity.schema.TapTable;
 import io.tapdata.pdk.apis.context.TapConnectorContext;
+import io.tapdata.pdk.apis.entity.message.ConsumeParam;
 import io.tapdata.pdk.apis.functions.connector.TapConnectorFunction;
 
 public interface ConsumeMessageFunction extends TapConnectorFunction {
-    void consumeMessage(TapConnectorContext connectorContext, TapTable tapTable, Long timestamp) throws Throwable;
+    void consumeMessage(TapConnectorContext connectorContext, TapTable tapTable, ConsumeParam consumeParam) throws Throwable;
 }
